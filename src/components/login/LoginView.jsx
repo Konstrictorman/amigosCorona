e6477fc2597a84424bc6749f45036ea9ee0737ab
Ricardo Álvarez <ralvarez@insolpr.com>
2022-08-26 22:18:55 -0500

    Cambios cosméticos al loginPage

--- src/components/login/LoginView.jsx
+++ src/components/login/LoginView.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-	Button,
-	Stack,
-	Typography,
-} from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Corona_logo from "../../assets/images/centroCorona.png";
 import LoginIcon from "@mui/icons-material/Login";
@@ -24,19 +20,25 @@ export const LoginView = () => {
 						justifyContent="flex-start"
 						alignItems="center"
 					>
-						<img
-							src={Corona_logo}
-							alt="Logo"
-							className="mt-5 mb-5 ml-2 pl-2 center login_logo"
-						/>
-
-						<Typography variant="h2" className="center mt-5" >
+						{" "}
+						<div className="login_logo_container">
+							<img
+								src={Corona_logo}
+								alt="Logo"
+								className="login_logo"
+							/>
+						</div>
+                  
+						<Typography variant="h2" className="center mt-5">
 							CC FIDELIZACIÓN
 						</Typography>
-						<Typography variant="caption" className="center mt-0" sx={{color:"whitesmoke"}}>
+						<Typography
+							variant="subtitle1"
+							className="center mt-0"
+							sx={{ color: "whitesmoke" }}
+						>
 							Bienvenido al programa de fidelización amigos Corona
 						</Typography>
-
 						<Button
 							onClick={handleClick}
 							variant="contained"
